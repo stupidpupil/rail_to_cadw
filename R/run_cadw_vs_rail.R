4766b285ac7fcd1b147e3c83fbe203d4f48bc180
Adam Watkins <adam@stupidpupil.co.uk>
2022-06-13 10:13:20 +0100

    Try adding National Museum Sites to process

--- R/run_cadw_vs_rail.R
+++ R/run_cadw_vs_rail.R
@@ -2,15 +2,23 @@ run_cadw_vs_rail <- function() {
 
   download_walesish_stuff()
 
-  cadw_sites <- get_cadw_sites()
-  unlink("output/cadw_sites.geojson")
-  cadw_sites %>% sf::write_sf("output/cadw_sites.geojson")
+  cadw_sites <- get_cadw_sites() %>%
+    mutate(operator = "cadw")
+
+  nat_mus_sites <- get_national_museum_sites() %>%
+    mutate(operator = "national_museum_wales")
+
+  sites <- cadw_sites %>%
+    bind_rows(nat_mus_sites)
+
+  unlink("output/sites.geojson")
+  sites %>% sf::write_sf("output/sites.geojson")
 
   origins <- get_origins()
   unlink("output/origins.geojson")
   origins %>% sf::write_sf("output/origins.geojson")
 
-  generate_ttms(origins, cadw_sites)
+  generate_ttms(origins, sites)
 
   write_ttms_as_json()
 }
